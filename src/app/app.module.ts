c00a037a950ee7b9d51a0120ff88f3a464eab8e0
Naya Hafez <nayoshca@gmail.com>
2019-05-12 00:31:35 +0300

    update template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { DemoComponent } from './demo/demo.component';
+
 import { NavComponent } from './nav/nav.component';
 import { IntroComponent } from './intro/intro.component';
 import { ContactComponent } from './contact/contact.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { QutufService } from './shared/utils/qutuf-service.service'
-import { ApiService } from './shared/utils/api-service.service'
+import { ApiService } from './shared/utils/api-service.service';
+import { HomeComponent } from './home/home.component'
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DemoComponent,
     NavComponent,
     IntroComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
